5c47a8622bb0afea8102d68e729acccfc5cd20fc
Seongil lee <modolee.si@gmail.com>
2021-02-10 01:31:32 +0900

    애드픽 광고 슬라이드형으로 변경

--- src/components/Ads/AdPick/ad-pick.container.tsx
+++ src/components/Ads/AdPick/ad-pick.container.tsx
@@ -2,24 +2,20 @@ import React from 'react';
 import AdPickView from './ad-pick.view';
 
 export default () => {
-  const adPickData = [
+  const banners = [
     {
-      url: 'https://bitl.kr/HP2Bhd',
-      img: './ads/mario_case.png',
+      img: './ads/coupang-erato.png',
+      url: 'https://bitl.kr/XFIkhO',
     },
     {
-      url: 'https://bitl.kr/jIoCaA',
+      img: './ads/mario-case.png',
+      url: 'https://bitl.kr/HP2Bhd',
+    },
+    {
       img: './ads/wingbling.png',
+      url: 'https://bitl.kr/jIoCaA',
     },
   ];
 
-  const getRandomIntInclusive = (min: number, max: number) => {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min; //최댓값도 포함, 최솟값도 포함
-  };
-
-  const adIndex = getRandomIntInclusive(0, adPickData.length - 1);
-
-  return <AdPickView img={adPickData[adIndex].img} url={adPickData[adIndex].url} />;
+  return <AdPickView banners={banners} />;
 };
--- src/components/Ads/AdPick/ad-pick.view.tsx
+++ src/components/Ads/AdPick/ad-pick.view.tsx
@@ -1,11 +1,31 @@
 import React from 'react';
+import { Slide } from 'react-slideshow-image';
+import 'react-slideshow-image/dist/styles.css';
+import './index.css';
 
-export default ({ url, img }: { url: string; img: string }) => {
+export default ({ banners }: { banners: { img: string; url: string }[] }) => {
+  const properties = {
+    duration: 1000,
+    transitionDuration: 500,
+    infinite: true,
+    arrows: false,
+    easing: 'ease',
+  };
+
   return (
-    <div style={{ textAlign: 'center' }}>
-      <a href={url} target="_blank">
-        <img src={img} style={{ width: '100%', maxWidth: '960px' }} />
-      </a>
+    <div className="slide-wrapper">
+      <Slide {...properties}>
+        {banners.map(banner => (
+          <div className="each-slide">
+            <div
+              style={{ backgroundImage: `url(${banner.img})` }}
+              onClick={() => {
+                window.open(banner.url, '_blank');
+              }}
+            />
+          </div>
+        ))}
+      </Slide>
     </div>
   );
 };
