ad07381c2455bf81c9f14531f5bdcca782e03e63
Seongil lee <modolee.si@gmail.com>
2021-02-18 16:55:28 +0900

    광고 업데이트

--- src/components/Ads/AdPick/ad-pick.container.tsx
+++ src/components/Ads/AdPick/ad-pick.container.tsx
@@ -4,32 +4,36 @@ import AdPickView from './ad-pick.view';
 export default () => {
   const banners = [
     {
+      img: './ads/db_soil.png', // DB손해보험 (3500)
+      url: 'https://nefing.com/64b5189',
+    },
+    {
+      img: './ads/cookie_kingdom.png', // 쿠키런 (2200)
+      url: 'https://nefing.com/246684f',
+    },
+    {
+      img: './ads/kb_electro.png', // 소액적금 (120)
+      url: 'https://nefing.com/433eaf4',
+    },
+    {
+      img: './ads/coway.png', // 커피값 정수기 (70)
+      url: 'https://nefing.com/6918edd',
+    },
+    {
+      img: './ads/dewit.png', // 듀잇 밴드 (60)
+      url: 'https://nefing.com/82ac564',
+    },
+    {
+      img: './ads/real_water.png', // 미세플라스틱 필터 (60)
+      url: 'https://nefing.com/7a33888',
+    },
+    {
       img: './ads/hackers_toefl.png',
       url: 'https://nefing.com/466faa2',
     },
     {
       img: './ads/hackers_police.png',
       url: 'https://nefing.com/239a4eb',
-    },
-    {
-      img: './ads/10000recipe.png',
-      url: 'https://bitl.kr/WVLjQP',
-    },
-    {
-      img: './ads/casamom.png',
-      url: 'https://bitl.kr/YnvrFU',
-    },
-    {
-      img: './ads/coupang-erato.png',
-      url: 'https://bitl.kr/XFIkhO',
-    },
-    {
-      img: './ads/mario-case.png',
-      url: 'https://bitl.kr/HP2Bhd',
-    },
-    {
-      img: './ads/wingbling.png',
-      url: 'https://bitl.kr/jIoCaA',
     },
   ];
 
